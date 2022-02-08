6e576f90565cdf26c435738c75bbd308b5a344e3
Jose Von Chong <javch1995@gmail.com>
2022-02-08 18:15:11 -0500

    challenges along promises vs callbacks vs async/await

---
+++ src/promise/index.js
@@ -0,0 +1,41 @@
+
+//Estructurando una promesa
+const somethingWillHappen = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Hey! all Good');
+        } else {
+            reject('Whoops!');
+        }
+    });
+};
+
+somethingWillHappen()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve('True');
+            }, 2000)
+        } else {
+            const error = new Error('oopss!');
+            reject(error);
+        }
+    });
+}
+
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => {
+        console.log('Array of results', response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
